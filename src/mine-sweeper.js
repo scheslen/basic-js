9c144d2f98df16672be4cfa88eb8354b46960f52
scheslen <antgalin@mail.ru>
2023-12-25 00:06:19 +0300

    feat: implement tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let aRes= [];
+  kMax=matrix.length;
+  lMax=matrix[0].length;
+
+  for (let i=0; i<kMax; i++){
+    aRes[i]=[];
+    for (let j=0; j<lMax; j++)
+    aRes[i][j] = 0;
+  }
+
+  let qM=0;
+  for (let k=0; k<kMax; k++){
+    for (let l=0; l<lMax; l++){
+      qM=0;
+      if (k>0 && l>0 && matrix[k-1][l-1]=== true) qM++ ;
+      if (k>0 && matrix[k-1][l]=== true) qM++ ;
+      if (k>0 && l<lMax && matrix[k-1][l+1]=== true) qM++ ;
+
+      if (l>0 && matrix[k][l-1]=== true) qM++ ;
+      if (l<lMax && matrix[k][l+1]=== true) qM++ ;
+
+      if (k<kMax-1 && l>0 && matrix[k+1][l-1]=== true) qM++ ;
+      if (k<kMax-1 && matrix[k+1][l]=== true) qM++ ;
+      if (k<kMax-1 && l<lMax && matrix[k+1][l+1]=== true) qM++ ;
+
+      aRes[k][l] = qM;
+  }
+}
+  
+  return aRes;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+ 
+  let qD=String(n).split('').map(v => Number(v)).reduce ((qN,d) => qN + d, 0);
+ 
+  while(qD>9){
+      qD=String(qD).split('').map(v => Number(v)).reduce ((qN,d) => qN + d, 0);
+  }
+
+ return qD
 }
 
 module.exports = {
